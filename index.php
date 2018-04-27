35a9d5f226393d5d3660b87eb61f978b622de08a
chiou <bohemianwind@gmail.com>
2018-04-27 13:10:34 +0800

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Homepage
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Example site</title>
+    <meta charset="utf-8">
+
+</head>
+<body>
+    <h1>Home</h1>
+</body>
+</html>
